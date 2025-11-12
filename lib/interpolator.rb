4c79e426ea9dcc90ced75db396bc21644cdfb1b9
Martin Vysny <mavi@vaadin.com>
2025-11-12 18:24:57 +0200

    interpolator

---
+++ lib/interpolator.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# All classes in this module have a [:value] function which calculates a value.
+module Interpolator
+  # Always provides given `value` {Object}.
+  class Const < Data.define(:value)
+  end
+
+  # Returns `value_from` if current time is less than `time_from`; `value_from` if current time is
+  # greater than `time_to`; a linear interpolation between `value_from` and `value_to` otherwise.
+  #
+  # Both `value_from` and `value_to` must be {Numeric} (ideally {Float}); both `time_from` and `time_from`
+  # must be {Time}.
+  class Linear < Data.define(:value_from, :value_to, :time_from, :time_to)
+    def initialize(hash)
+      super(hash)
+      unless value_from.is_a?(Numeric) && value_to.is_a?(Numeric) && time_from.is_a?(Time) && time_to.is_a?(Time)
+        raise 'invalid value type'
+      end
+      raise "#{time_from} can't be later than #{time_to}" if time_from > time_to
+    end
+
+    def value
+      now = Time.now
+      return value_from if now < time_from
+      return value_to if now > time_to
+
+      value_from + (value_to - value_from) * (now - time_from) / (time_to - time_from)
+    end
+  end
+end
---
+++ test/test_interpolator.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'interpolator'
+require 'timecop'
+
+class TestInterpolator < Minitest::Test
+  def test_const
+    assert_equal 2, Interpolator::Const.new(2).value
+    assert_equal 'q', Interpolator::Const.new('q').value
+  end
+
+  def test_linear
+    assert_equal 2.0, Interpolator::Linear.new(2.0, 10.0, Time.now + 5, Time.now + 10).value
+    assert_equal 10.0, Interpolator::Linear.new(2.0, 10.0, Time.now - 10, Time.now - 5).value
+    now = Time.now
+    # Fix `Time.now` during the duration of the test
+    Timecop.travel(now) do
+      assert_equal 6.0, Interpolator::Linear.new(2.0, 10.0, now - 5, now + 5).value.round(2)
+      assert_equal 0.0, Interpolator::Linear.new(2.0, -2.0, now - 5, now + 5).value.round(2)
+    end
+  end
+end
